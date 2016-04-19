f9cd2ef12f6b425605a7afcdc88a5cc0ca8f640d
ehwang-nr <elizabeth.hwang@nativerank.com>
2016-04-19 10:55:48 -0600

    calcuator function working

--- src/js/script.js
+++ src/js/script.js
@@ -1,43 +1,51 @@
 $(document).ready(function(){
+
+    var estimate = 0;
+    var input = $('#budget_value');
+    var budget = 0;
+
+    function compare(a,b){
+
+        if(a < b){
+            $('.uk-width-3-10').append('<div class="uk-alert uk-alert-danger" id="over"> This is over your budget!<div class="uk-alert-close uk-close" id="close"></div></div>');
+                $('#close').click(function(){
+                    $('#over').fadeOut();
+            })
+        }else{
+            $('.uk-alert uk-alert-danger').hide();
+        }
+    };
+
     $('#budget_form').submit(function(){
 
-        var input = $('#budget_value').val();
-        $('#budget').append("<h4>" + input + "</h4>");
-        
+        budget = input.val();
+        $('#budget').html("$" + budget + ".00");
+
+        compare(budget, estimate);
+
+        input.val(" ");
         return false;
 
     });
 
-    var estimate = 0;
 
+    $('#loc_count').change(function(){
 
-   // $('#loc_count').change(function(){
-   //
-   //     var count = $('#loc_count').val();
-   //     estimate += count * 1200;
-   //
-   //     $('#estimate').html("<h3>$" + estimate + ".00</h3>");
-   //
-   // });
+       var count = $('#loc_count').val();
+        estimate = (count * 1200);
 
-    if($('$loc_count').change()){
-        var count = $('#loc_count').val();
-        estimate += count * 1200;
+       $('#estimate').html("$" + estimate + ".00");
 
-        $('#estimate').html("<h3>$" + estimate + ".00</h3>");
+    compare(budget, estimate);
+   });
 
-    }
-
-    if($('#expand_web').val() === 'yes'){
-        
+    $('#expand_web').change(function() {
+        if($('#expand_web').val() == "yes") {
             var count = $('#loc_count').val();
-            estimate = count * 3588;
-            $('#estimate').html("<h3>$" + estimate + ".00</h3>");
+            var estimate =  count * 3588;
+            $('#estimate').html("$" + estimate + ".00");
         }
-
+        compare(budget, estimate);
     });
-
-
     
-
 })
