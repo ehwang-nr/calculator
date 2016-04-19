d7241be403f31e1938fd1a7d2befcff89415f7ba
ehwang-nr <elizabeth.hwang@nativerank.com>
2016-04-19 14:00:34 -0600

    reorganized questions

--- src/js/script.js
+++ src/js/script.js
@@ -28,24 +28,78 @@ $(document).ready(function(){
 
     });
 
+ //Type of service section
+
+    $('#service_type').change(function() {
+        if($('#service_type').val() == "local") {
+            $('#local-1').fadeIn('slow');
+            $('#national-1').css('display', 'none');
+
+        }
+        else if($('#service_type').val() == "national"){
+            $('#national-1').fadeIn('slow');
+            $('#local-1').css('display', 'none');
+            $('#local-2').css('display', 'none');
+        }
+    })
+
+    $('#local-1').change(function() {
+        $('#local-2').fadeIn('slow');
+    })
+
+
 
     $('#loc_count').change(function(){
 
        var count = $('#loc_count').val();
-        estimate = (count * 1200);
+        estimate = count * 1200;
 
        $('#estimate').html("$" + estimate + ".00");
 
-    compare(budget, estimate);
+        compare(budget, estimate);
    });
 
     $('#expand_web').change(function() {
         if($('#expand_web').val() == "yes") {
             var count = $('#loc_count').val();
-            var estimate =  count * 3588;
+            estimate =  count * 3588;
             $('#estimate').html("$" + estimate + ".00");
         }
         compare(budget, estimate);
     });
+
+
+ //New Website Section
+
+    $('#new_web').change(function() {
+        if($(this).val() == "yes") {
+            $('#web-1').fadeIn('slow');
+        }
+    })
+
+
+    $('#template').change(function() {
+        if ($(this).val() == "yes") {
+            estimate += 5000;
+            $('#estimate').html("$" + estimate + ".00");
+            $('#web-3').fadeIn('slow');
+
+        } else if ($(this).val() == "no") {
+            $('#web-2').fadeIn('slow');
+        }
+    })
+
+
+    $('#restaurant').change(function() {
+        if ($(this).val() == "yes") {
+            estimate = (estimate-5000) + 4000;
+            $('#estimate').html("$" + estimate + ".00");
+            $('#web-3').fadeIn('slow');
+
+        } else if ($('#template').val() == "no") {
+            $('#web-2').fadeIn('slow');
+        }
+    })
+
     
 })
