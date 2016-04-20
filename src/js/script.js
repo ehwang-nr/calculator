1c23918877abfbf39d5e7f2355bade42b4367864
ehwang-nr <elizabeth.hwang@nativerank.com>
2016-04-20 09:33:48 -0600

    changed theme. cleaned up folders

--- src/js/script.js
+++ src/js/script.js
@@ -4,26 +4,28 @@ $(document).ready(function(){
     var template = 0;
     var estimate = 0;
 
-    function calcEstimate (){
-        estimate = loc_count + template;
-       // return estimate;
-        $('#estimate').html("$" + estimate + ".00");
-
-    }
     var input = $('#budget_value');
     var budget = 0;
 
     function compare(a,b){
 
         if(a < b){
-            $('#warning').html('<div class="uk-alert uk-alert-danger" id="over"> This is over your budget!<div class="uk-alert-close uk-close" id="close"></div></div>');
-                $('#close').click(function(){
-                    $('#over').fadeOut();
+            $('#warning').html('<div class="uk-alert uk-alert-danger" id="over"> This is over your budget!<button class="uk-alert-close uk-close" id="close"></button></div>');
+            $('#close').click(function(){
+                $('#over').fadeOut();
             })
         }else{
             $('.uk-alert uk-alert-danger').hide();
         }
     };
+
+    function calcEstimate (){
+        estimate = loc_count + template;
+
+        $('#estimate').html("$" + estimate + ".00");
+        compare(budget, estimate);
+    }
+
 
     $('#budget_form').submit(function(){
 
@@ -62,8 +64,6 @@ $(document).ready(function(){
        var count = $('#loc_count').val();
         loc_count = count * 1200;
         calcEstimate();
-        //$('#estimate').html("$" + estimate + ".00");
-        compare(budget, estimate);
    });
 
     $('#expand_web').change(function() {
@@ -71,11 +71,9 @@ $(document).ready(function(){
             var count = $('#loc_count').val();
             loc_count =  count * 3588;
             calcEstimate();
-          //  $('#estimate').html("$" + estimate + ".00");
         }else{
             loc_count =  $('#loc_count').val() * 1200;
             calcEstimate();
-          //  $('#estimate').html("$" + estimate + ".00");
         }
     });
 
@@ -93,16 +91,13 @@ $(document).ready(function(){
         if ($(this).val() == "yes") {
             template = 5000;
             calcEstimate();
-         //   $('#estimate').html("$" + estimate + ".00");
-            compare(budget, estimate);
             $('#web-3').fadeIn('slow');
 
         } else if ($(this).val() == "no") {
             template = 0;
             calcEstimate();
-          //  $('#estimate').html("$" + estimate + ".00");
-            compare(budget, estimate);
             $('#web-2').fadeIn('slow');
+            $('#web-3').fadeOut('slow');
         }
     })
 
@@ -111,15 +106,11 @@ $(document).ready(function(){
         if ($(this).val() == "yes") {
             template = (template-5000) + 4000;
             calcEstimate();
-          //  $('#estimate').html("$" + estimate + ".00");
-            compare(budget, estimate);
             $('#web-3').fadeIn('slow');
 
         } else if ($(this).val() == "no") {
             template = 5000;
             calcEstimate();
-         //   $('#estimate').html("$" + estimate + ".00");
-            compare(budget, estimate);
             $('#web-2').fadeIn('slow');
         }
     })
